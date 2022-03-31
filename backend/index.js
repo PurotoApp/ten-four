f0ff902e6001e675f4fbce04a5d5d5960e1df4fb
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-03-31 23:58:56 +0200

    Init upload

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const routing = require("./routing_sys.js");
+require('dotenv').config()
+
+app.use((req, res, next) => {
+    console.log(`New request`);
+    next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+const path = require('path')
+app.use('/public', express.static(path.join(__dirname, 'frontend')))
+
+app.use('', routing)
+
+app.listen(process.env.PORT, function() {
+    console.log(`App is running on ${process.env.PORT}`);
+    console.log(`We are currently in a ${process.env.STATE} enviorment`)
+});
---
+++ backend/routing_sys.js
@@ -0,0 +1,23 @@
+module.exports = (function() {
+    var route = require('express').Router();
+    const path = require('path');
+    const express = require('express')
+
+    route.get('/', function(req, res, next) {
+        res.sendFile(path.join(__dirname, '/frontend/index.html'));
+    });
+
+    route.post('/api/send/', function(req, res, next) {
+        if (req.query.msg == undefined) {
+            res.sendFile(path.join(__dirname, '/frontend/index.html')); // add error page later
+        }
+        console.log(req.body.msg)
+        res.redirect('/');
+    });
+
+    route.use((req, res, next) => {
+        res.sendFile(path.join(__dirname, '/frontend/404.html'));
+    });
+
+    return route;
+})();
