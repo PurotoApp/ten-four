5e52adff6626f876cab5b88cbc72d786aadf265a
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-04-06 00:04:09 +0200

    Got H capcha working

--- backend/routing_sys.js
+++ backend/routing_sys.js
@@ -1,6 +1,6 @@
 module.exports = (function() {
     var route = require('express').Router();
-    const hcaptcha = require('express-hcaptcha');
+    const { verify } = require('hcaptcha');
     const path = require('path');
     const express = require('express')
 
@@ -8,11 +8,20 @@ module.exports = (function() {
         res.sendFile(path.join(__dirname, '../frontend/index.html'));
     });
 
-    route.post('/api/send/', hcaptcha.middleware.validate(process.env.HCAPTCHA_TOKEN), function(req, res, next) {
+    route.post('/api/send/', function(req, res, next) {
         if (req.query.msg == undefined) {
             res.sendFile(path.join(__dirname, '../frontend/index.html')); // add error page later
         }
-        console.log(req.body.msg)
+        console.log(req.body["h-captcha-response"])
+        verify(process.env.HCAPTCHA_TOKEN, req.body["h-captcha-response"])
+            .then((data) => {
+                if (data.success === true) {
+                    console.log('success!', data);
+                } else {
+                    console.log('verification failed');
+                }
+            })
+            .catch(console.error);
         res.redirect('/');
     });
 
